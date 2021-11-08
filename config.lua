21eb56cefe716af1a9f5006944b1726b9645d6b0
WRIGH7Y <92814197+WRIGH7Y@users.noreply.github.com>
2021-11-08 13:36:32 +0000

    Added burgershot

--- config.lua
+++ config.lua
@@ -6,5 +6,6 @@ Config.Jobs = {
     ['realestate'] = vector3(-124.786, -641.486, 168.820),
     ['taxi'] = vector3(903.32, -170.55, 74.0),
     ['cardealer'] = vector3(-32.0, -1114.2, 26.4),
-    ['mechanic'] = vector3(-347.41, -133.48, 39.01)
+    ['mechanic'] = vector3(-347.41, -133.48, 39.01),
+    ['burgershot'] = vector3(-1192.04, -902.476, 13.998),
 }
